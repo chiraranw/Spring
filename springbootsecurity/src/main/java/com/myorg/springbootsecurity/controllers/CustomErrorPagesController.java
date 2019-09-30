84aada8e94105c64238c031c35b9f528ba0eab27
Nation Chirara <chiraranw@gmail.com>
2019-09-30 14:20:18 +0200

    Dealing with the / on methods & return

--- springbootsecurity/src/main/java/com/myorg/springbootsecurity/controllers/CustomErrorPagesController.java
+++ springbootsecurity/src/main/java/com/myorg/springbootsecurity/controllers/CustomErrorPagesController.java
@@ -27,6 +27,7 @@ public class CustomErrorPagesController implements ErrorController {
         return "/error/";
     }
 
+    //With ThymLeaf - when you put "/",don't put them in the return!
     @RequestMapping("/error")
     public String handleError(HttpServletRequest request) {
         Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
@@ -35,11 +36,11 @@ public class CustomErrorPagesController implements ErrorController {
             Integer statusCode = Integer.valueOf(status.toString());
 
             if (statusCode == HttpStatus.NOT_FOUND.value()) {
-                return "/error/error-404";
+                return "error/error-404";
             } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                return "/error/error-500";
+                return "error/error-500";
             }else if (statusCode == HttpStatus.FORBIDDEN.value()) {
-                return "/error/error-403";
+                return "error/error-403";
             }
         }
         return "error/error";
